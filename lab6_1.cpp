ccfd4839be9d69cf591cfdfabeb496c4b3859247
ShibaNoi <evergreen19832@gmail.com>
2025-12-12 02:16:27 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int n;
+    int evenCount = 0;
+    int oddCount = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+
+    while (n != 0) {       
+
+        if (n % 2 == 0)
+            evenCount++;   
+        else
+            oddCount++;    
+
+        cout << "Enter an integer: ";
+        cin >> n;
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
